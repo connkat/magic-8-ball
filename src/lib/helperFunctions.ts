d22d5d0ee93083f1a9028fa76db7ea1bb543d2a7
connkat <hi.connkat@gmail.com>
2022-09-29 17:59:53 -0600

    options

--- src/lib/helperFunctions.ts
+++ src/lib/helperFunctions.ts
@@ -1,4 +1,4 @@
-const answers = [];
+const answers = ["Yes", "No", "Ask again later", "Definitely", "Absolutely not", "50/50", "Ask your mom"];
 
 export function findAnswer(): string | void{
 //ranmdonize selection
