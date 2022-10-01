79ae0d7900e599b77c1885fe9dfc1efe63932a29
connkat <hi.connkat@gmail.com>
2022-09-30 23:24:00 -0600

    randomized pick

--- src/lib/helperFunctions.ts
+++ src/lib/helperFunctions.ts
@@ -1,5 +1,14 @@
-const answers = ["Yes", "No", "Ask again later", "Definitely", "Absolutely not", "50/50", "Ask your mom"];
+const answers = [
+  "Yes",
+  "No",
+  "Ask again later",
+  "Definitely",
+  "Absolutely not",
+  "50/50",
+  "Ask your mom",
+];
 
-export function findAnswer(): string | void{
-//ranmdonize selection
+export function findAnswer(): string | void {
+  const random = Math.floor(Math.random() * max) + 7;
+  return answers[random];
 }
